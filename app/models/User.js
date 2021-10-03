a0d0088f43762229b9093213f9747fda3560aed2
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-03 20:25:51 +0300

    Create User model

---
+++ app/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const Sneaker = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    permissionID: {
+        type: Number,
+        required: true,
+    },
+})
+
+export default mongoose.model('User', Sneaker)
