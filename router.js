a15c21de85e59b39098d6202a386777c2f2a6cb3
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-17 16:09:31 +0300

    Add routes for admin sneakers

--- router.js
+++ router.js
@@ -4,18 +4,24 @@ import validate, { isNewUser, isPair } from './app/middlewares/validation.js';
 /** Controllers */
 import AuthController from './app/controllers/AuthController.js';
 import SneakerController from './app/controllers/SneakerController.js';
+import adminController from "./app/controllers/adminController.js";
 
 const router = express.Router();
 
 /** Auth Routes */
 router.post('/auth/register', validate(isNewUser), AuthController.registerUser);
 router.post('/auth/login', AuthController.authentication);
-router.post('/auth/check-in', AuthController.checkAuth);
+router.post('/auth/check-in', AuthController.checkAuth, (req, res) => {
+  res.status(200).json(res.locals.tokenData);
+});
 
 /** Sneakers Routes */
 router.get(['/sneakers', '/sneakers/:id'], SneakerController.getSneakers);
 router.post('/sneakers', validate(isPair), SneakerController.postSneakers);
 router.put('/sneakers/:id', SneakerController.updateSneakers);
 router.delete('/sneakers/:id', SneakerController.deleteSneakers);
+
+/** Admin Routes */
+router.get('/admin/sneakers', AuthController.checkAuth, adminController.getSneakers);
 
 export default router;
