77ba63c011936a9a499d47e1889b13ed6e240e13
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-17 21:01:37 +0300

    Adding id in getting not approved sneakers

--- app/services/admin/AdminService.js
+++ app/services/admin/AdminService.js
@@ -1,11 +1,13 @@
 import Sneaker from "../../models/Sneaker.js";
-import { prepareImageLink } from "../../../utils.js";
+import { prepareImageLink, serializeObjectId } from "../../../utils.js";
 
 class AdminService {
   async getNotApprovedSneakers() {
-    return prepareImageLink(await Sneaker.find({
+    const pairWithoutId = prepareImageLink(await Sneaker.find({
       isApproved: false,
     }));
+
+    return serializeObjectId(pairWithoutId);
   }
 }
 
