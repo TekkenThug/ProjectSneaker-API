3a59ec74055704c927c2e30ef3e37e57d18f88e2
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-17 22:22:52 +0300

    Send response if resolving is success

--- app/controllers/AdminController.js
+++ app/controllers/AdminController.js
@@ -12,7 +12,10 @@ class AdminController {
   async resolveSneakersApplication(req, res) {
     try {
       const { id, resolve } = req.body;
+
       await AdminService.resolveSneakersApplication(id, resolve);
+
+      res.json('Application resolved successfully');
     } catch (e) {
       res.send(500).json(e.message)
     }
