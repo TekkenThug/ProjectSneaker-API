a1fd3437291d056a9d5538a114a77c6a8ab87ee5
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-23 14:00:25 +0300

    Rework method for generate access tokens

--- app/libs/jwt.js
+++ app/libs/jwt.js
@@ -1,10 +1,21 @@
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import { v4 as uuid } from 'uuid';
+import Token from '../models/Token.js';
 
 dotenv.config();
 
-export const generateAccessToken = async (payload) => {
-  return jwt.sign(payload, process.env.API_JWT_TOKEN, { expiresIn: '1h' });
+export const generateTokens = async (id, payload) => {
+  const newRefreshToken = uuid();
+  await Token.create({
+    token: newRefreshToken,
+    userID: id,
+  });
+
+  return {
+    token: jwt.sign(payload, process.env.API_JWT_TOKEN, { expiresIn: '1h' }),
+    refreshToken: newRefreshToken,
+  };
 };
 
 export const checkAccessToken = async (token) => {
