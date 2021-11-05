7b430e0e0007e1ad5865c3ed225e6ab060287288
Vadim Ignatov <ignatov0131@gmail.com>
2021-11-05 23:07:52 +0300

    Refactor auth service

--- app/services/Auth.js
+++ app/services/Auth.js
@@ -12,40 +12,59 @@ const generatePayload = (user) => {
   };
 };
 
+/**
+ * @typedef {object} LogInData
+ * @property {string} email - user email
+ * @property {string} password - user password
+ */
+
 class Auth {
+  /**
+   * Register new user
+   * @param {object} data - potential new user data
+   * @returns {Promise<void>}
+   */
   async register(data) {
     const isExist = await User.findOne({
       email: data.email,
     });
 
-    if (!isExist && data.password === data.repeatPassword) {
-      const hash = await bcrypt.hash(data.password, SALT_ROUNDS);
-      await User.create({ ...data, passwordHash: hash, permissionID: 1 });
-    } else {
-      throw new Error('User with this email is registered');
-    }
+    if (isExist) throw new Error('User with this email is registered');
+
+    if (data.password !== data.repeatPassword) throw new Error('Password mismatch');
+
+    const hash = await bcrypt.hash(data.password, SALT_ROUNDS);
+    await User.create({ ...data, passwordHash: hash, permissionID: 1 });
   }
 
+  /**
+   * Authenticate user
+   * @param {LogInData} data - user login data
+   * @returns {JWTPair} - token pairs
+   */
   async authenticateUser(data) {
     const potentialUser = await User.findOne({
       email: data.email,
     });
 
-    if (potentialUser) {
-      if (await Token.findOne({ userID: potentialUser._id.toString() })) {
-        throw new Error('User already login');
-      }
+    if (!potentialUser) throw new Error('User with this email not found');
 
-      const match = await bcrypt.compare(data.password, potentialUser.passwordHash);
-
-      if (!match) throw new Error('Incorrect password');
-
-      return await generateTokens(potentialUser._id.toString(), generatePayload(potentialUser));
+    if (await Token.findOne({ userID: potentialUser._id.toString() })) {
+      throw new Error('User already login');
     }
 
-    throw new Error('User with this email not found');
+    const match = await bcrypt.compare(data.password, potentialUser.passwordHash);
+
+    if (!match) throw new Error('Incorrect password');
+
+    return await generateTokens(potentialUser._id.toString(), generatePayload(potentialUser));
   }
 
+  /**
+   * Update refresh token
+   * @param {string} refreshToken - user refresh token
+   * @returns {JWTPair} - token pair
+   */
   async updateRefreshToken(refreshToken) {
     const tokenRecord = await Token.findOne({ token: refreshToken });
 
@@ -56,12 +75,22 @@ class Auth {
     return await generateTokens(tokenRecord.userID, generatePayload(await User.findById(tokenRecord.userID)));
   }
 
+  /**
+   * Check authenticate user state
+   * @param {string|number} id - user id
+   * @returns {Promise<void>}
+   */
   async checkAuthStatus(id) {
     const tokenRecord = await Token.findOne({ userID: id });
 
     if (!tokenRecord) throw new Error('Invalid user');
   }
 
+  /**
+   * Logout user
+   * @param {string|number} id - user id
+   * @returns {Promise<void>}
+   */
   async logout(id) {
     const tokenRecord = await Token.findOne({ userID: id });
 
