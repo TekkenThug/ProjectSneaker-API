a3e5a70e65e5df14fd9f3e7b93f00a2a3f9e8486
Vadim Ignatov <ignatov0131@gmail.com>
2021-11-05 22:54:30 +0300

    Add jsdoc into admin service

--- app/services/admin/AdminService.js
+++ app/services/admin/AdminService.js
@@ -2,6 +2,10 @@ import Sneaker from '../../models/Sneaker.js';
 import { prepareImageLink, serializeObjectId } from '../../utils.js';
 
 class AdminService {
+  /**
+   * Get not approved application of sneakers
+   * @returns {Promise<Array>} Not approved sneakers
+   */
   async getNotApprovedSneakers() {
     const pairWithoutId = prepareImageLink(await Sneaker.find({
       isApproved: false,
@@ -10,6 +14,12 @@ class AdminService {
     return serializeObjectId(pairWithoutId);
   }
 
+  /**
+   * Resolving application of sneakers
+   * @param {string|number} id - pair ID
+   * @param {string|boolean} resolve - administrator resolving
+   * @returns {Promise<void>}
+   */
   async resolveSneakersApplication(id, resolve) {
     const pairIsApproved = (await Sneaker.findById(id, 'isApproved')).isApproved;
 
