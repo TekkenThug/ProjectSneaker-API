fb09b115934fbb70d8d419bc2d7d64d57b295ee0
Vadim Ignatov <ignatov0131@gmail.com>
2021-11-05 23:13:41 +0300

    Add jsdoc for sneakers service

--- app/services/sneakers/SneakerService.js
+++ app/services/sneakers/SneakerService.js
@@ -2,7 +2,19 @@ import Sneaker from '../../models/Sneaker.js';
 import { saveFile } from '../../libs/file.js';
 import { formatDate, prepareImageLink } from '../../utils.js';
 
+/**
+ * @typedef {object} SneakerSearchParams
+ * @property {string} model - model name
+ * @property {number} limit - limit for searching output
+ */
+
 class SneakerService {
+  /**
+   * Get sneakers
+   * @param {string|number|undefined} id - pair id for getting
+   * @param {SneakerSearchParams| {}} searchParams - params for search query
+   * @returns {Promise<Array|object>} - pair of sneakers
+   */
   async getSneakers(id, searchParams = {}) {
     if (!id) {
       const { model, limit } = searchParams;
@@ -20,6 +32,11 @@ class SneakerService {
     return prepareImageLink(sneakers);
   }
 
+  /**
+   * Create pair of sneakers
+   * @param {object} data - sneakers data
+   * @returns {Promise<void>}
+   */
   async createSneakers(data) {
     await Sneaker.create({
       ...data,
